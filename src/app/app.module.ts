84c44b163db243683cb8724dff02d13e248805c6
HelioGonzales <heliogonzalesr@gmail.com>
2023-08-28 23:01:16 +0200

    feat: implement footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/modules/header/components/header/header.module';
+import { FooterModule } from './shared/modules/footer/footer.module';
 @NgModule({
   declarations: [
     AppComponent,
-
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HeaderModule
+    HeaderModule,
+    FooterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
