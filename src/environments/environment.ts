d4d2c753baea9977427f765afe36551047711099
HelioGonzales <heliogonzalesr@gmail.com>
2023-12-22 22:24:43 +0100

    fix: deploy 2 cyclic backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // api: 'https://akllasumaq-backend.onrender.com/api/v1/'
-  api: 'https://teal-different-deer.cyclic.app/'
+  api: 'https://calm-dove-windbreaker.cyclic.app/api/v1/'
 };
