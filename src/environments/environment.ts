d9db95344af6e8331a2557c572a3618c86644fb4
HelioGonzales <heliogonzalesr@gmail.com>
2023-12-22 21:43:55 +0100

    feat: change for cyclic backend srv

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://akllasumaq-backend.onrender.com/api/v1/'
+  // api: 'https://akllasumaq-backend.onrender.com/api/v1/'
+  api: 'https://teal-different-deer.cyclic.app/'
 };
