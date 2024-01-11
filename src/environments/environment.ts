ea1d5cb0de137cfa6d102a7a0355af2a0fed920a
HelioGonzales <heliogonzalesr@gmail.com>
2024-01-11 21:48:12 +0100

    feat: back to render backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  // api: 'https://akllasumaq-backend.onrender.com/api/v1/'
-  api: 'https://calm-dove-windbreaker.cyclic.app/api/v1/'
+  api: 'https://akllasumaq-backend.onrender.com/api/v1/'
 };
