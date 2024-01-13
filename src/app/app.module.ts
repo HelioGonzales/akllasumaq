00f92e0f6913f41690a81d9bc110949f88dd518e
HelioGonzales <heliogonzalesr@gmail.com>
2024-01-13 22:37:05 +0100

    feat: start shopping cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderModule } from './shared/modules/header/components/header/header.module';
+import { JwtInterceptor } from './shared/services/jwt.interceptor';
 import { FooterModule } from './shared/modules/footer/footer.module';
 import { LoginModule } from './shared/modules/login/login.module';
-import { JwtInterceptor } from './shared/services/jwt.interceptor';
-import { CategoriesService } from './shared/services/categories.service';
+import { CartService } from './shared/services/cart.service';
 
 @NgModule({
   declarations: [
@@ -23,7 +23,11 @@ import { CategoriesService } from './shared/services/categories.service';
     FooterModule,
     LoginModule
   ],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }, CategoriesService],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(cartSvc: CartService) {
+    cartSvc.initCartLocalStorage()
+  }
+}
